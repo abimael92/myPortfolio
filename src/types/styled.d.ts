4f3689d8fdfabf22d610300e36765f1a831a0929
Abimael Garcia <abimael1992g@gmail.com>
2025-10-13 12:45:56 -0600

    chore: add TypeScript styled-components types

---
+++ src/types/styled.d.ts
@@ -0,0 +1,24 @@
+// src/types/styled.d.ts
+import 'styled-components';
+
+declare module 'styled-components' {
+	export interface DefaultTheme {
+		fonts: {
+			title: string;
+			main: string;
+		};
+		colors: {
+			primary1: string;
+			background1: string;
+			accent1: string;
+			button: string;
+			background2: string;
+		};
+		breakpoints: {
+			sm: string;
+			md: string;
+			lg: string;
+			xl: string;
+		};
+	}
+}

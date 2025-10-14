68d0f1b62e563e219db8caa8e2b43671c79ad960
Abimael Garcia <abimael1992g@gmail.com>
2025-10-14 14:05:16 -0600

    refactor: migrate Footer component to TypeScript

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,68 @@
+// src/components/Footer/Footer.tsx
+import React from 'react';
+import { AiFillGithub, AiOutlineMail, AiFillLinkedin } from 'react-icons/ai';
+import BlurWrapper from '../PrivateContent/BlurWrapper';
+import { useAuth } from '../../hooks/useAuth'; // Import custom hook
+import { SocialIcons } from '../Header/HeaderStyles';
+import {
+	CompanyContainer,
+	FooterWrapper,
+	LinkColumn,
+	LinkItem,
+	LinkList,
+	LinkTitle,
+	Slogan,
+	SocialContainer,
+	SocialIconsContainer,
+} from './FooterStyles';
+
+const Footer: React.FC = () => {
+	const { accessToken } = useAuth(); // Use custom hook instead of useContext
+	const hasAccess = !!accessToken;
+
+	return (
+		<FooterWrapper>
+			<LinkList>
+				<LinkColumn>
+					<LinkTitle>Call</LinkTitle>
+					<BlurWrapper authenticated={hasAccess}>
+						<LinkItem href='tel:614-132-5405'>(614)-132-5405</LinkItem>
+					</BlurWrapper>
+				</LinkColumn>
+				<LinkColumn>
+					<LinkTitle>Email</LinkTitle>
+					<BlurWrapper authenticated={hasAccess}>
+						<LinkItem href='mailto:abimael1992g@gmail.com'>
+							abimael1992g@gmail.com
+						</LinkItem>
+					</BlurWrapper>
+				</LinkColumn>
+			</LinkList>
+
+			<SocialIconsContainer>
+				<CompanyContainer>
+					<Slogan>
+						My Portfolio
+						<br />
+						Abimael Garcia (Web Developer)
+					</Slogan>
+				</CompanyContainer>
+				<SocialContainer>
+					<BlurWrapper authenticated={hasAccess}>
+						<SocialIcons href='http://github.com/abimael92' target='_blank' rel='noopener noreferrer'>
+							<AiFillGithub size='3rem' />
+						</SocialIcons>
+						<SocialIcons href='https://www.linkedin.com/in/abimael-garcia-00580314a/' target='_blank' rel='noopener noreferrer'>
+							<AiFillLinkedin size='3rem' />
+						</SocialIcons>
+						<SocialIcons href='mailto:abimael1992g@gmail.com' target='_blank' rel='noopener noreferrer'>
+							<AiOutlineMail size='3rem' />
+						</SocialIcons>
+					</BlurWrapper>
+				</SocialContainer>
+			</SocialIconsContainer>
+		</FooterWrapper>
+	);
+};
+
+export default Footer;

273fa9f4486ad9754975179f873c9ec30507a06e
Abimael Garcia <abimael1992g@gmail.com>
2025-10-14 14:05:17 -0600

    refactor: migrate NavDropDown component to TypeScript

---
+++ src/components/NavDropDown/index.tsx
@@ -0,0 +1,24 @@
+// src/components/NavDropDown/index.tsx
+import React from 'react';
+import { AiFillPhone, AiOutlineMail } from 'react-icons/ai';
+import { FaLocationArrow } from "react-icons/fa";
+import {
+  DropDownContainer,
+  DropDownIcon,
+  DropDownItem,
+  DropDownItemDesc,
+  DropDownItemTitle,
+  DropDownTextContainer
+} from './NavDropDown';
+
+interface NavDropDownProps {
+  active?: boolean;
+}
+
+const NavDropDown: React.FC<NavDropDownProps> = (props) => (
+  <div>
+    NavDropDown
+  </div>
+);
+
+export default NavDropDown;

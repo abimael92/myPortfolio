cc2632f8a2cc2e29225f71bc96f845d10d2d0ae8
Abimael Garcia <abimael1992g@gmail.com>
2025-11-10 19:27:01 -0600

    refactor(footer): update Footer component structure and logic

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,6 +5,7 @@ import BlurWrapper from '../PrivateContent/BlurWrapper';
 import { useAuth } from '../../hooks/useAuth'; // Import custom hook
 import { SocialIcons } from '../Header/HeaderStyles';
 import {
+	Container,
 	CompanyContainer,
 	FooterWrapper,
 	LinkColumn,
@@ -21,47 +22,49 @@ const Footer: React.FC = () => {
 	const hasAccess = !!accessToken;
 
 	return (
-		<FooterWrapper>
-			<LinkList>
-				<LinkColumn>
-					<LinkTitle>Call</LinkTitle>
-					<BlurWrapper authenticated={hasAccess}>
-						<LinkItem href='tel:614-132-5405'>(614)-132-5405</LinkItem>
-					</BlurWrapper>
-				</LinkColumn>
-				<LinkColumn>
-					<LinkTitle>Email</LinkTitle>
-					<BlurWrapper authenticated={hasAccess}>
-						<LinkItem href='mailto:abimael1992g@gmail.com'>
-							abimael1992g@gmail.com
-						</LinkItem>
-					</BlurWrapper>
-				</LinkColumn>
-			</LinkList>
+		<Container>
+			<FooterWrapper>
+				<LinkList>
+					<LinkColumn>
+						<LinkTitle>Call</LinkTitle>
+						<BlurWrapper authenticated={hasAccess}>
+							<LinkItem href='tel:614-132-5405'>(614)-132-5405</LinkItem>
+						</BlurWrapper>
+					</LinkColumn>
+					<LinkColumn>
+						<LinkTitle>Email</LinkTitle>
+						<BlurWrapper authenticated={hasAccess}>
+							<LinkItem href='mailto:abimael1992g@gmail.com'>
+								abimael1992g@gmail.com
+							</LinkItem>
+						</BlurWrapper>
+					</LinkColumn>
+				</LinkList>
 
-			<SocialIconsContainer>
-				<CompanyContainer>
-					<Slogan>
-						My Portfolio
-						<br />
-						Abimael Garcia (Web Developer)
-					</Slogan>
-				</CompanyContainer>
-				<SocialContainer>
-					<BlurWrapper authenticated={hasAccess}>
-						<SocialIcons href='http://github.com/abimael92' target='_blank' rel='noopener noreferrer'>
-							<AiFillGithub size='3rem' />
-						</SocialIcons>
-						<SocialIcons href='https://www.linkedin.com/in/abimael-garcia-00580314a/' target='_blank' rel='noopener noreferrer'>
-							<AiFillLinkedin size='3rem' />
-						</SocialIcons>
-						<SocialIcons href='mailto:abimael1992g@gmail.com' target='_blank' rel='noopener noreferrer'>
-							<AiOutlineMail size='3rem' />
-						</SocialIcons>
-					</BlurWrapper>
-				</SocialContainer>
-			</SocialIconsContainer>
-		</FooterWrapper>
+				<SocialIconsContainer>
+					<CompanyContainer>
+						<Slogan>
+							My Portfolio
+							<br />
+							Abimael Garcia (Web Developer)
+						</Slogan>
+					</CompanyContainer>
+					<SocialContainer>
+						<BlurWrapper authenticated={hasAccess}>
+							<SocialIcons href='http://github.com/abimael92' target='_blank' rel='noopener noreferrer'>
+								<AiFillGithub size='3rem' />
+							</SocialIcons>
+							<SocialIcons href='https://www.linkedin.com/in/abimael-garcia-00580314a/' target='_blank' rel='noopener noreferrer'>
+								<AiFillLinkedin size='3rem' />
+							</SocialIcons>
+							<SocialIcons href='mailto:abimael1992g@gmail.com' target='_blank' rel='noopener noreferrer'>
+								<AiOutlineMail size='3rem' />
+							</SocialIcons>
+						</BlurWrapper>
+					</SocialContainer>
+				</SocialIconsContainer>
+			</FooterWrapper>
+		</Container>
 	);
 };
 
